6a28f9aed157820e46a75b69f0003c2a8b6e7263
Kevin Nobel <kevin@2sk.nl>
2021-02-11 23:28:45 +0100

    Updated screenshot script.

--- screenshots.py
+++ screenshots.py
@@ -1,40 +1,37 @@
-# Name:         shootscreen.py
+# Name:         screenshots.py
 # Author:       ~tweska
-# Description:  Take a screenshot of all personal tilde club pages
-#               that have been changed.
+# Description:  Take a screenshot of all personal tilde club pages.
 
+import sys
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from PIL import Image
 
-from scrapenames import users
-
 BASE_URL = 'https://tilde.club/'
-SCREENSHOT_PATH = 'screenshots/'
+OUTPUT_PATH = 'out/screenshots/'
 
 options = webdriver.ChromeOptions()
 options.headless = True
 driver = webdriver.Chrome(options=options)
 driver.set_window_size(800, 600)
 
-def shootscreen(username, verbose=True):
-    url = "{}{}".format(BASE_URL, username)
-    path = "{}{}.png".format(SCREENSHOT_PATH, username[1:])
-    
+# Read usernames from stdin.
+for line in sys.stdin:
+    user = line.strip()
+    url = "{}~{}".format(BASE_URL, user)
+    path = "{}{}.png".format(OUTPUT_PATH, user)
+
     print("Taking screenshot of: '{}'".format(url))
-    
+
     try:
         driver.get(url)
         driver.get_screenshot_as_file(path)
     except:
         print("Error occured while taking screenshot of: '{}'".format(url))
-        return
+        continue
     
     img = Image.open(path)
     img = img.resize((240, 180))
     img.save(path)
-
-for user in users:
-    shootscreen(user)
 
 driver.quit()
